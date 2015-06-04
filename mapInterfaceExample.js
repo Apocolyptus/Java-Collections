add1a189ecc9f12c5c0960adf24f21c415c125df
Sterling <Apocolyptus@users.noreply.github.com>
2015-06-04 16:27:03 -0700

    Create mapInterfaceExample.js

---
+++ mapInterfaceExample.js
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class CollectionsDemo {
+
+   public static void main(String[] args) {
+      Map m1 = new HashMap(); 
+      m1.put("Zara", "8");
+      m1.put("Mahnaz", "31");
+      m1.put("Ayan", "12");
+      m1.put("Daisy", "14");
+      System.out.println();
+      System.out.println(" Map Elements");
+      System.out.print("\t" + m1);
+   }
+}
+
+//which produces
+
+/*
+
+Map Elements
+        {Mahnaz=31, Ayan=12, Daisy=14, Zara=8}
+
+*/
